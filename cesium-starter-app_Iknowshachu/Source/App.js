a32cd6f6a71149559b26b1d4d87edd5fff409974
nsaito <naooooooo@msk-web.co.jp>
2015-07-04 11:02:54 +0900

    KML URL Chenge
    BUG

--- cesium-starter-app_Iknowshachu/Source/App.js
+++ cesium-starter-app_Iknowshachu/Source/App.js
@@ -2,4 +2,4 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 
 viewer.camera.viewRectangle(Cesium.Rectangle.fromDegrees(114.591,-45.837,148.970,-5.730));
 
-viewer.dataSources.add(Cesium.KmlDataSource.load('http://naooooooo.github.io/20150704test/cesium-starter-app_Iknowshachu/cesium-starter-app-Ikowshachu/Source/20150704KML.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://naooooooo.github.io/20150704test/cesium-starter-app_Iknowshachu/Source/20150704KML.kml'));
