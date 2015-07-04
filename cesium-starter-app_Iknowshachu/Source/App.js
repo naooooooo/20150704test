1c38f9ac143b9f856edab1380f613bee4f0d57b0
nsaito <naooooooo@msk-web.co.jp>
2015-07-04 10:57:25 +0900

    Add KML
    KMLファイルの追加

--- cesium-starter-app_Iknowshachu/Source/App.js
+++ cesium-starter-app_Iknowshachu/Source/App.js
@@ -1,3 +1,6 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
 
 viewer.camera.viewRectangle(Cesium.Rectangle.fromDegrees(114.591,-45.837,148.970,-5.730));
+
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://seabook0303.github.io/2015070
+4test/cesium-starter-app-Ikowshachu/Source/20150704KML.kml'));
